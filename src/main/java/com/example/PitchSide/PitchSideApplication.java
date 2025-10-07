6ba69c51f5c4510f0e2e648bd0d57a6594095e9e
SamueleTermine <Samuele.termine@itsrizzoli.it>
2025-10-07 20:19:59 +0200

    Initial commit

---
+++ src/main/java/com/example/PitchSide/PitchSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.PitchSide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PitchSide/PitchSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PitchSide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
